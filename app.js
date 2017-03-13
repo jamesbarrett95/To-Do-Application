f5cd6870153df475affbbe6194adfa24bb6f3ab6
jamesbarrett95 <james-barrett@hotmail.com>
2017-03-13 23:09:28 +0000

    comments added before next build session

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const submit = document.getElementById("submit");
 const entry = document.getElementById("todoentry");
 const buttonSection = document.getElementById("buttons");
 
+// Create To-Do List
 function createToDoList(appName) {
 
+  // Create a list item
   function addListItem(input) {
     const li = document.createElement("li");
     const span = document.createElement("span");
     const editButton = document.createElement("button");
     const removeButton = document.createElement("button");
-    editButton.id = "edit";
-    removeButton.id = "removeButton";
     editButton.textContent = "Edit";
     removeButton.textContent = "Remove";
     span.textContent = input;
@@ -23,15 +23,19 @@ function createToDoList(appName) {
     todoentry.value = "";
   }
 
+  // Remove all elements within the container to begin the creation of the To-Do App
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
+
   container.appendChild(appName);
 
-  let ul = document.createElement("ul");
+  const ul = document.createElement("ul");
+  ul.id = "listItems";
   container.appendChild(ul);
+  listItems = document.getElementById("listItems");
 
-  let addItemInput = document.createElement("input");
+  const addItemInput = document.createElement("input");
 
   addItemInput.setAttribute("type", "text");
   addItemInput.setAttribute("placeholder", "Enter To-Do Here!")
@@ -40,17 +44,19 @@ function createToDoList(appName) {
   container.appendChild(addItemInput);
   addItemInput.focus();
 
-  let addButton = document.createElement("button");
+  const addButton = document.createElement("button");
 
   addButton.className = "addButton";
   addButton.id = "addButton";
   addButton.textContent = "Add!";
   container.appendChild(addButton);
 
+  // Add list item when clicking the 'Add' button
   addButton.addEventListener("click", (event) => {
     addListItem(addItemInput.value);
   });
 
+  // Add list item when pressing enter
   todoentry.addEventListener("keyup", (event) => {
     event.preventDefault();
     if(event.keyCode === 13) {
@@ -59,29 +65,33 @@ function createToDoList(appName) {
   })
 }
 
+// Create app
 function createAppName(input) {
   let nameOfToDo = document.createElement("p");
   nameOfToDo.textContent = input;
   createToDoList(nameOfToDo);
 }
 
-input.addEventListener("keyup", function(event) {
+// If user presses enter when naming their app, crete the app
+input.addEventListener("keyup", (event) => {
   event.preventDefault();
   if(event.keyCode == 13) {
     createAppName(input.value);
   }
 });
 
+// Create the app depending on what value is given to the app name
 submit.addEventListener("click", (event) => {
   if(input.value === "") {
     let error = document.createElement("p");
     p.textContent = "Please enter a value";
-    container.appendChild(p)
+    container.appendChild(p);
   } else {
     createAppName(input.value);
   }
 });
 
+// Create the app if choosing one of the pre-defined options
 buttonSection.addEventListener("click", (event) => {
   if(event.target.tagName === "BUTTON") {
     let option = event.target.firstElementChild;
